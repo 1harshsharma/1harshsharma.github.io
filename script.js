f95d08ca509f4eb4aeb8b8e2c2bc19082521c0bc
1harshsharma <153484023+1harshsharma@users.noreply.github.com>
2024-02-11 17:25:32 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('name').addEventListener('mouseover', function() {
+    document.querySelector('.full-name').style.display = 'inline';
+  });
+  
+  document.getElementById('name').addEventListener('mouseout', function() {
+    document.querySelector('.full-name').style.display = 'none';
+  });
+  
