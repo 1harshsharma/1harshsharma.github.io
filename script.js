8415e208855866c7f68574eee5df49a0a3e6b60a
1harshsharma <153484023+1harshsharma@users.noreply.github.com>
2024-02-12 15:08:35 +1100

    basic information update

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('name').addEventListener('mouseover', function() {
-    document.querySelector('.full-name').style.display = 'inline';
-  });
-  
-  document.getElementById('name').addEventListener('mouseout', function() {
-    document.querySelector('.full-name').style.display = 'none';
-  });
-  
